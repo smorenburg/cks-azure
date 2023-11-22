e06be978803cc2298424ff108d31206a3c039346
Robin Smorenburg <robin@robino.io>
2023-11-22 09:43:48 +0100

    Add second node

--- terraform/azure/virtual_machine.tf
+++ terraform/azure/virtual_machine.tf
@@ -58,7 +58,7 @@ resource "azurerm_linux_virtual_machine" "control_001" {
   }
 }
 
-# Create the network interface and public IP address for the node.
+# Create the network interfaces and public IP addresses for the nodes.
 resource "azurerm_network_interface" "node_001" {
   name                = "nic-vm-node-${local.suffix}-001"
   location            = var.location
@@ -80,7 +80,28 @@ resource "azurerm_public_ip" "node_001" {
   domain_name_label   = "vm-node-${local.suffix}-001"
 }
 
-# Create the node.
+resource "azurerm_network_interface" "node_002" {
+  name                = "nic-vm-node-${local.suffix}-002"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.default.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.default.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.node_002.id
+  }
+}
+
+resource "azurerm_public_ip" "node_002" {
+  name                = "pip-vm-node-${local.suffix}-002"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.default.name
+  allocation_method   = "Dynamic"
+  domain_name_label   = "vm-node-${local.suffix}-002"
+}
+
+# Create the nodes.
 resource "azurerm_linux_virtual_machine" "node_001" {
   name                = "vm-node-${local.suffix}-001"
   location            = var.location
@@ -101,6 +122,43 @@ resource "azurerm_linux_virtual_machine" "node_001" {
 
   os_disk {
     name                   = "osdisk-vm-node-${local.suffix}-001"
+    caching                = "ReadWrite"
+    storage_account_type   = "Standard_LRS"
+    disk_encryption_set_id = azurerm_disk_encryption_set.default.id
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts-gen2"
+    version   = "latest"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "node_002" {
+  name                = "vm-node-${local.suffix}-002"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.default.name
+  computer_name       = "node-002"
+  size                = "Standard_D2s_v5"
+  admin_username      = "adminuser"
+  secure_boot_enabled = true
+
+  network_interface_ids = [
+    azurerm_network_interface.node_002.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = tls_private_key.ssh.public_key_openssh
+  }
+
+  os_disk {
+    name                   = "osdisk-vm-node-${local.suffix}-002"
     caching                = "ReadWrite"
     storage_account_type   = "Standard_LRS"
     disk_encryption_set_id = azurerm_disk_encryption_set.default.id
